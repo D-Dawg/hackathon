8a037007b75326db83751b50e387a3af05a11f8e
dennis <dennis.sommer.86@gmail.com>
2017-04-06 10:57:15 +0200

    done

--- view/ChromeExtension/js/background.js
+++ view/ChromeExtension/js/background.js
@@ -1,16 +1,10 @@
-var data = 0;
+var data = null;
 chrome.runtime.onMessage.addListener(function (msg, sender) {
 
     // First, validate the message's structure
     if ((msg.from === 'content') && msg.data != null) {
         data = msg.data;
-
-        // Enable the page-action for the requesting tab
-        // window.alert(msg.data);
-        // chrome.pageAction.show(sender.tab.id);
-        // chrome.runtime.sendMessage({
-        //     from:    'background',
-        //     data: jsonLDAnnotation
-        // });
+    }else {
+        data = null;
     }
 });
--- view/ChromeExtension/js/content.js
+++ view/ChromeExtension/js/content.js
@@ -2,14 +2,17 @@ $(document).ready(function() {
 var REGEX_JSONLD = /<script type="application\/ld\+json">(.*)<\/script>/g;
 var pageSource = $( 'html' ).html().toString();
 var jsonLDAnnotation = pageSource.match(REGEX_JSONLD);
+var request = null;
     if(jsonLDAnnotation != null){
         window.alert("Hey Semanti here, I found an annotation on this website, can I help?");
         jsonLDAnnotation = jsonLDAnnotation[0].substring('<script type="application/ld+json">'.length,jsonLDAnnotation[0].indexOf('</script>'));
-        chrome.runtime.sendMessage({
-            from:    'content',
-            data: jsonLDAnnotation
-        });
+        request = jsonLDAnnotation;
     }
+    chrome.runtime.sendMessage({
+        from:    'content',
+        data: request
+    });
+
 });
 
 
