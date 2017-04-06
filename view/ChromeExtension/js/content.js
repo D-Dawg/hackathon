60194a9bbd5a61dd3915e1006cdc97d47955631e
dennis <dennis.sommer.86@gmail.com>
2017-04-06 04:24:04 +0200

    clientChanges

--- view/ChromeExtension/js/content.js
+++ view/ChromeExtension/js/content.js
@@ -2,8 +2,10 @@ $(document).ready(function() {
 var REGEX_JSONLD = /<script type="application\/ld\+json">(.*)<\/script>/g;
 var pageSource = $( 'html' ).html().toString();
 var jsonLDAnnotation = pageSource.match(REGEX_JSONLD);
-    jsonLDAnnotation = jsonLDAnnotation[0].substring('<script type="application/ld+json">'.length,jsonLDAnnotation[0].indexOf('</script>'));
-
+    if(jsonLDAnnotation != null){
+        window.alert("Found a jsonLD.");
+        jsonLDAnnotation = jsonLDAnnotation[0].substring('<script type="application/ld+json">'.length,jsonLDAnnotation[0].indexOf('</script>'));
+    }
 });
 
 
